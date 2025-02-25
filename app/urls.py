23325fa48e756ab3cf4c4da102ea6341037913fc
GrAz1p <alihantorebekov@gmail.com>
2025-02-25 13:30:18 +0600

    fixed error

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 urlpatterns = [
     path('create/user/', views.create_user),
     path('get/users/', views.get_user),
-    path('get/user/<int:id>/', views.get_user_by_id),
-    path('update/user/<int:id>/', views.update_user),
-    path('delete/user/<int:id>/', views.delete_user),
+    path('get/user/<int:user_id>/', views.get_user_by_id),
+    path('update/user/<int:user_id>/', views.update_user),
+    path('delete/user/<int:user_id>/', views.delete_user),
 ]
